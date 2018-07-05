1134e31c2d31c9c0273baaaa2c245fd0984ac59d
zoo-chee <ratstail0725@gmail.com>
2018-07-05 22:40:04 +0900

    SQLiteでDBとテーブルを作成できるようになった。

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from mod.scraping.Scraper import Scraper
+from mod.sqlite.SqliteService import SqliteService
 
 #url = "https://github.com/zoo-chee/scraping"
 url = "https://raw.githubusercontent.com/zoo-chee/scraping/master/test/test.md"
 scraper = Scraper(url)
 scraper.query()
 scraper.print_response()
+
+service = SqliteService()
+service.createDb()
+service.commit()
